62d8ad8f28c7095580194e0ce4d48b70ac12810e
Roy Hsu <roy.hsu@tinyworld.cc>
2018-09-09 15:49:28 +0800

    add the Xcode project.

---
+++ Sources/TinyValidation.h
@@ -0,0 +1,12 @@
+//
+//  TinyValidation.h
+//  TinyValidation
+//
+//  Created by Roy Hsu on 2018/9/9.
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double TinyValidationVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char TinyValidationVersionString[];
