faac7eaa72e634d48bfc3d1c320f45695e72d874
Roy Hsu <roy.hsu@tinyworld.cc>
2018-09-09 19:07:36 +0800

    make Int, Float, Double conformt to Validatable.

---
+++ Sources/Numberic+Validatable.swift
@@ -0,0 +1,14 @@
+//
+//  Numberic+Validatable.swift
+//  TinyValidation
+//
+//  Created by Roy Hsu on 2018/9/9.
+//
+
+// MARK: - Numberic
+
+extension Int: Validatable { }
+
+extension Float: Validatable { }
+
+extension Double: Validatable { }
