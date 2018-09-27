9a35bbe20ab17aaa92006503fe0091448b73a423
Roy Hsu <roy.hsu@tinyworld.cc>
2018-09-27 22:07:40 +0800

    add documentation for optional validation.

--- Sources/Optional+Validatable.swift
+++ Sources/Optional+Validatable.swift
@@ -10,12 +10,28 @@
 // swiftlint:disable syntactic_sugar
 public extension Optional {
 
+    /// Similiar to explicitlyValidated(by rule:)
     @discardableResult
     public func explicitlyValidated(
         by validator: (Optional<Wrapped>) throws -> Wrapped
     )
     rethrows -> Wrapped { return try validator(self) }
 
+    /// If the nil value is one of requirements to validate, and you don't want to skip the validation by optional chaining.
+    /// Please use these two explicit validation methods.
+    ///
+    /// Example:
+    ///
+    /// ```
+    /// var message: String? = nil
+    ///
+    /// // The validation rule will be triggered.
+    /// let result: String = try message.explicitlyValidated(by: rule)
+    ///
+    /// // The validation will be skipped due to optional chaining
+    /// let result: String? = try message?.validated(by: rule)
+    /// ```
+    ///
     @discardableResult
     public func explicitlyValidated<R>(by rule: R) throws -> Wrapped
     where
